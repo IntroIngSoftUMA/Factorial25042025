ab3f8e69a2cced427f2a9037de9b5ade79d92d97
ajnebro <ajnebro@users.noreply.github.com>
2025-04-09 09:45:42 +0200

    Refactor method compute()

--- src/main/java/org/iis/Factorial.java
+++ src/main/java/org/iis/Factorial.java
@@ -3,14 +3,8 @@ package org.iis;
 public class Factorial {
   public int compute(int value) {
     int result;
-    if ((value == 0) || (value == 1)) {
+    if (value == 0) {
       result = 1;
-    } else if (value == 2) {
-      result = 2;
-    } else if (value == 3) {
-      result = 6;
-    } else if (value == 4) {
-      result = 24;
     } else {
       result = value * compute(value - 1);
     }
